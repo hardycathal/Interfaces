1fb05a9b4b244b247a26a35d1d5b30b72d34dbe0
hardycathal <G00424572@atu.ie>
2024-02-08 11:32:54 +0000

    Created an Interface called MenuItem and an app called OnlineOrderingSystem

---
+++ src/main/java/ie/atu/OnlineOrderingSystem.java
@@ -0,0 +1,24 @@
+package ie.atu;
+
+public class OnlineOrderingSystem {
+    public static void main(String[] args) {
+        // Create an array or list of MenuItem objects
+        MenuItem[] menuItems = new MenuItem[]{
+                // Add menu items
+        };
+
+        // Display details of each menu item
+        for (MenuItem menuItem : menuItems){
+            // Print out the Name, price and description of each item
+
+        }
+
+        // Calculate and display the total cost of the order
+        double totalCost = 0;
+        for (MenuItem menuItem : MenuItems){
+            // totalCost of all menuItem prices in an order
+        }
+        System.out.println("Toatl Cost: €" + totalCost);
+    }
+
+}
